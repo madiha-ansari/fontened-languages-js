f9c42cd5e593138155123247bed4133e5152c5b6
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-02-11 08:18:27 +0500

    started the java script languages

---
+++ app.js
@@ -0,0 +1,32 @@
+// ch no 1 "Alert"
+// let myName = ("Madiha Ansari");
+// alert(myName)
+
+// window.alert("Thank you")
+
+// ch no 2 
+// Variables for Strings
+// var nationallity = "Pakistani"
+// console.log(nationallity);
+// console.log(typeof(nationallity));
+
+// var myFirstName = "Madiha Ansari"
+// myFirstName = "Ali Huzaifa"
+// console.log(myFirstName);
+
+// ch no 3
+// Variables for Numbers
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum);
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum);
+
+// ch no 4
+// Variable Names Legal and Illegal
+
+// var myName = "hello"  //are Legal 
+// var alert             // are Illegal 
